e2c9a923f078be05d4aa9d3bba41da85b86830f4
BABULAL MANDAL <babulalmandal678@gmail.com>
2024-09-06 23:35:23 +0530

    First React Project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import KgButton from './KgButton';
+import Hello from './Hello';
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>This is the React Component</h1>
+      <KgButton></KgButton>
+      <Hello></Hello>
+    </>
+  )
+}
+
+export default App
---
+++ src/Hello.jsx
@@ -0,0 +1,6 @@
+function Hello(){
+  let myName = 'Babulal'
+  return <h3>This is the future Speaking. My Name is {myName}</h3>
+}
+
+export default Hello;
